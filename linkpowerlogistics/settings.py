0af7ab6d09429c46bfa7c3d8a9e8193f093da27d
Jhonny <jhonnykellerdev@gmail.com>
2023-01-27 20:19:38 +0000

    'finalchanges'

--- linkpowerlogistics/settings.py
+++ linkpowerlogistics/settings.py
@@ -89,15 +89,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
         'default' : {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'linkpowerdb',
