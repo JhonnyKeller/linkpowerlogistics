1bee158184ecac584762834d8c44603ab18512ff
Jhonny <jhonnykellerdev@gmail.com>
2023-01-27 20:08:52 +0000

    'changingdb'

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,8 @@ from .models import information
 # Register your models here.
 
 class InfoAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request, obj=None):
-        return False
+    # def has_add_permission(self, request, obj=None):
+    #     return False
     # This will help you to disable delete functionaliyt
     def has_delete_permission(self, request, obj=None):
         return False
